1f2919e079fa5533b7d4f4c7460f26080f7d7d81
Willi Kappler <willi.kappler.gm@gmail.com>
2016-08-15 16:00:55 +0200

    fix tsp example

--- examples/tsp/src/main.rs
+++ examples/tsp/src/main.rs
@@ -6,6 +6,7 @@
 // But they can get very close
 
 extern crate rand;
+#[macro_use] extern crate lazy_static;
 
 // Internal crates
 extern crate darwin_rs;
@@ -19,6 +20,31 @@ use darwin_rs::individual::Individual;
 use darwin_rs::simulation_builder;
 use darwin_rs::population_builder;
 
+lazy_static! {
+    static ref CITY_POSITIONS : Vec<(f64, f64)> =
+
+        vec![(2.852197810188428, 90.31966506130796),
+              (33.62874999956513, 44.9790462485413),
+              (22.064901432163996, 83.9172876840628),
+              (20.595912954825923, 12.798762916676043),
+              (42.2234133639806, 88.41646877787616),
+              (94.18533963242542, 21.151217108254627),
+              (25.84671166792939, 63.707153428189514),
+              (13.051898250315553, 89.61945656056766),
+              (76.41370000896038, 97.20491253636689),
+              (18.832993288649792, 6.006559110093601),
+              (96.98045791932294, 72.23019966333018),
+              (71.93203564171793, 93.03998204972012),
+              (33.39161715459793, 5.13372283892819),
+              (25.23072873231501, 67.1123015383591),
+              (84.38812085016241, 90.80055533944926),
+              (29.20345964254656, 21.17642854392676),
+              (58.11390834674495, 66.93322778502613),
+              (22.070195932187254, 59.73489434853766),
+              (86.29060211377086, 83.14129496517567),
+              (55.760857794890796, 26.95947234362994)];
+}
+
 fn city_distance(city: &[(f64, f64)], index1: usize, index2: usize) -> f64 {
     let (x1, y1) = city[index1];
     let (x2, y2) = city[index2];
@@ -30,39 +56,16 @@ fn city_distance(city: &[(f64, f64)], index1: usize, index2: usize) -> f64 {
 
 #[derive(Debug, Clone)]
 struct CityItem {
-    city_positions: Vec<(f64, f64)>,
-    path: Vec<usize>,
+    path: Vec<usize>
 }
 
 // Implement trait functions mutate and calculate_fitness:
 impl Individual for CityItem {
     fn new() -> CityItem {
-        let city_positions = vec![(2.852197810188428, 90.31966506130796),
-                                  (33.62874999956513, 44.9790462485413),
-                                  (22.064901432163996, 83.9172876840628),
-                                  (20.595912954825923, 12.798762916676043),
-                                  (42.2234133639806, 88.41646877787616),
-                                  (94.18533963242542, 21.151217108254627),
-                                  (25.84671166792939, 63.707153428189514),
-                                  (13.051898250315553, 89.61945656056766),
-                                  (76.41370000896038, 97.20491253636689),
-                                  (18.832993288649792, 6.006559110093601),
-                                  (96.98045791932294, 72.23019966333018),
-                                  (71.93203564171793, 93.03998204972012),
-                                  (33.39161715459793, 5.13372283892819),
-                                  (25.23072873231501, 67.1123015383591),
-                                  (84.38812085016241, 90.80055533944926),
-                                  (29.20345964254656, 21.17642854392676),
-                                  (58.11390834674495, 66.93322778502613),
-                                  (22.070195932187254, 59.73489434853766),
-                                  (86.29060211377086, 83.14129496517567),
-                                  (55.760857794890796, 26.95947234362994)];
-
-        let mut path: Vec<usize> = (0..city_positions.len()).map(|x| x as usize).collect();
+        let mut path: Vec<usize> = (0..CITY_POSITIONS.len()).map(|x| x as usize).collect();
         path.push(0); // Add start position to end of path
 
         CityItem {
-            city_positions: city_positions,
             path: path,
         }
     }
@@ -70,12 +73,12 @@ impl Individual for CityItem {
     fn mutate(&mut self) {
         let mut rng = rand::thread_rng();
         // Keep stating position always the same:
-        let index1: usize = rng.gen_range(1, self.city_positions.len());
-        let mut index2: usize = rng.gen_range(1, self.city_positions.len());
+        let index1: usize = rng.gen_range(1, CITY_POSITIONS.len());
+        let mut index2: usize = rng.gen_range(1, CITY_POSITIONS.len());
 
         // Small optimisation
         while index1 == index2 {
-            index2 = rng.gen_range(1, self.city_positions.len());
+            index2 = rng.gen_range(1, CITY_POSITIONS.len());
         }
 
         // Here we just swap the two indices. Compare this to example/tsp2 where we have
@@ -86,11 +89,11 @@ impl Individual for CityItem {
 
     // fitness means here: the length of the route, the shorter the better
     fn calculate_fitness(&self) -> f64 {
-        let mut prev_index = &(self.city_positions.len() - 1);
+        let mut prev_index = &(CITY_POSITIONS.len() - 1);
         let mut length: f64 = 0.0;
 
         for index in &self.path {
-            length += city_distance(&self.city_positions, *prev_index, *index);
+            length += city_distance(&CITY_POSITIONS, *prev_index, *index);
 
             prev_index = index;
         }
@@ -171,9 +174,8 @@ fn main() {
 
             println!("Path and coordinates: ");
 
-            let cities = &tsp_simulation.simulation_result.fittest[0].individual.city_positions;
             for index in &tsp_simulation.simulation_result.fittest[0].individual.path {
-                let (x, y) = cities[*index];
+                let (x, y) = CITY_POSITIONS[*index];
                 println!("{} {}", x, y);
             }
 
