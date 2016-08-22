5093f154d4d4792357913d6a08b357d43f8da8df
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-08-23 00:23:39 +0200

    add clean and update to bash script for each example

--- run_examples.sh
+++ run_examples.sh
@@ -3,22 +3,32 @@
 cd examples
 
 cd ocr
+cargo clean
+cargo update
 cargo run --release
 cd ..
 
 cd queens
+cargo clean
+cargo update
 cargo run --release
 cd ..
 
 cd sudoku
+cargo clean
+cargo update
 cargo run --release
 cd ..
 
 cd tsp
+cargo clean
+cargo update
 cargo run --release
 cd ..
 
 cd tsp2
+cargo clean
+cargo update
 cargo run --release
 cd ..
 
