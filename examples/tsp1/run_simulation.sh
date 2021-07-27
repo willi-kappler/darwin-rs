92c93b40e27dd455cd2a2135d63582e7e687e467
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-07-27 17:40:39 +0200

    Add clean up code

--- examples/tsp1/run_simulation.sh
+++ examples/tsp1/run_simulation.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-target/release/tsp1 -s &
+killall tsp1
+
+rm *.log *.dat
+
+# best may be: 376.3341189874508
+target/release/tsp1 -l 400.0 -s &
 
 sleep 2
 
@@ -8,7 +13,7 @@ target/release/tsp1 -m 5 &
 target/release/tsp1 -m 10 &
 target/release/tsp1 -m 15 &
 target/release/tsp1 -m 20 &
-#target/release/tsp1 -m 25 &
-#target/release/tsp1 -m 30 &
-#target/release/tsp1 -m 35 &
-#target/release/tsp1 -m 40 &
+target/release/tsp1 -m 25 &
+target/release/tsp1 -m 30 &
+target/release/tsp1 -m 35 &
+target/release/tsp1 -m 40 &
