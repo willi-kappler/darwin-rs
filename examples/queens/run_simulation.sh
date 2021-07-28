e09633cf6bafc43a7359bd575fb8e41135d986a9
Willi Kappler <willi.kappler.gm@gmail.com>
2021-07-28 22:04:56 +0200

    Increase number of iteration for better performance

--- examples/queens/run_simulation.sh
+++ examples/queens/run_simulation.sh
@@ -8,11 +8,12 @@ target/release/queens -l 1.0 -s &
 
 sleep 2
 
-target/release/queens -m 1 &
-target/release/queens -m 2 &
-target/release/queens -m 4 &
-target/release/queens -m 8 &
-target/release/queens -m 16 &
-target/release/queens -m 32 &
-target/release/queens -m 64 &
-target/release/queens -m 128 &
+target/release/queens -m 1 -i 50000 &
+target/release/queens -m 2 -i 50000 &
+target/release/queens -m 4 -i 50000 &
+target/release/queens -m 8 -i 50000 &
+# target/release/queens -m 16 -i 50000 &
+# target/release/queens -m 32 -i 50000 &
+# target/release/queens -m 64 -i 50000 &
+# target/release/queens -m 128 -i 50000 &
+
