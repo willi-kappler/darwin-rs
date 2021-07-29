c45de6528027c22be5bd9bfa76736bfcda928e63
Willi Kappler <willi.kappler.gm@gmail.com>
2021-07-29 15:18:58 +0200

    Add small delay for each node

--- examples/queens/run_simulation.sh
+++ examples/queens/run_simulation.sh
@@ -4,16 +4,24 @@ killall queens
 
 rm *.log *.dat
 
+DELAY=0.1
+
 target/release/queens -l 1.0 -s &
 
 sleep 2
 
 target/release/queens -m 1 -i 50000 &
+sleep $DELAY
 target/release/queens -m 2 -i 50000 &
+sleep $DELAY
 target/release/queens -m 4 -i 50000 &
+sleep $DELAY
 target/release/queens -m 8 -i 50000 &
+sleep $DELAY
 # target/release/queens -m 16 -i 50000 &
+#sleep $DELAY
 # target/release/queens -m 32 -i 50000 &
+#sleep $DELAY
 # target/release/queens -m 64 -i 50000 &
+#sleep $DELAY
 # target/release/queens -m 128 -i 50000 &
-
--- examples/tsp2/run_simulation.sh
+++ examples/tsp2/run_simulation.sh
@@ -4,16 +4,26 @@ killall tsp2
 
 rm *.log *.dat
 
+DELAY=0.1
+
 # best may be: 376.3341189874508
 target/release/tsp2 -l 400.0 -s &
 
 sleep 2
 
 target/release/tsp2 -m 1 -i 50000 &
+sleep $DELAY
 target/release/tsp2 -m 2 -i 50000 &
+sleep $DELAY
 target/release/tsp2 -m 4 -i 50000 &
+sleep $DELAY
 target/release/tsp2 -m 8 -i 50000 &
+sleep $DELAY
 #target/release/tsp2 -m 25 &
+#sleep $DELAY
 #target/release/tsp2 -m 30 &
+#sleep $DELAY
 #target/release/tsp2 -m 35 &
+#sleep $DELAY
 #target/release/tsp2 -m 40 &
+
