ee131bfbbb4863c0ca801b0163554485787e36bc
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-08-09 14:14:44 +0200

    Add possible best limit, increase number of iterations in run bash script

--- examples/tsp3/run_simulation.sh
+++ examples/tsp3/run_simulation.sh
@@ -6,23 +6,23 @@ rm *.log *.dat
 
 DELAY=0.1
 
-# best may be: 376.3341189874508
+# best may be: 91379.51073418828
 target/release/tsp3 -l 400.0 -s &
 
 sleep 2
 
-target/release/tsp3 -m 1 -i 50000 &
+target/release/tsp3 -m 1 -i 500000 &
 sleep $DELAY
-target/release/tsp3 -m 2 -i 50000 &
+target/release/tsp3 -m 2 -i 500000 &
 sleep $DELAY
-target/release/tsp3 -m 4 -i 50000 &
+target/release/tsp3 -m 4 -i 500000 &
 sleep $DELAY
-target/release/tsp3 -m 8 -i 50000 &
+target/release/tsp3 -m 8 -i 500000 &
 sleep $DELAY
-#target/release/tsp3 -m 16 -i 50000 &
+#target/release/tsp3 -m 16 -i 500000 &
 #sleep $DELAY
-#target/release/tsp3 -m 32 -i 50000 &
+#target/release/tsp3 -m 32 -i 500000 &
 #sleep $DELAY
-#target/release/tsp3 -m 64 -i 50000 &
+#target/release/tsp3 -m 64 -i 500000 &
 #sleep $DELAY
-#target/release/tsp3 -m 128 -i 50000 &
+#target/release/tsp3 -m 128 -i 500000 &
