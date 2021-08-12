04a1f2c627df0b440d80c406dd1354e17a8eb8be
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-08-12 12:15:22 +0200

    Set new defaults

--- examples/tsp3/run_simulation.sh
+++ examples/tsp3/run_simulation.sh
@@ -6,23 +6,23 @@ rm *.log *.dat
 
 DELAY=0.1
 
-# best may be: 90878.5569909252
-target/release/tsp3 -l 90880.0 -s &
+# best may be: 89971.50164001813
+target/release/tsp3 -l 89980.0 -s &
 
 sleep 2
 
-target/release/tsp3 -m 10 -i 10000 -o 10 &
+target/release/tsp3 -m 10 -i 1000 -o 100 &
 sleep $DELAY
-target/release/tsp3 -m 10 -i 10000 -o 100 &
+target/release/tsp3 -m 20 -i 1000 -o 100 &
 sleep $DELAY
-target/release/tsp3 -m 10 -i 100000 -o 10 &
+target/release/tsp3 -m 30 -i 1000 -o 100 &
 sleep $DELAY
-target/release/tsp3 -m 10 -i 100000 -o 100 &
+target/release/tsp3 -m 40 -i 1000 -o 100 &
 sleep $DELAY
-target/release/tsp3 -m 100 -i 10000 -o 10 &
+target/release/tsp3 -m 50 -i 1000 -o 100 &
 sleep $DELAY
-target/release/tsp3 -m 100 -i 10000 -o 100 &
+target/release/tsp3 -m 60 -i 1000 -o 100 &
 sleep $DELAY
-target/release/tsp3 -m 100 -i 100000 -o 10 &
+target/release/tsp3 -m 70 -i 1000 -o 100 &
 sleep $DELAY
-target/release/tsp3 -m 100 -i 100000 -o 100 &
+target/release/tsp3 -m 80 -i 1000 -o 100 &
