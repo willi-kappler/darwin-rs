68a80e1864ef0eb97fbb43017c1ac1bbcb4fbcf7
Willi Kappler <willi.kappler.gm@gmail.com>
2021-08-15 16:52:25 +0200

    Change default run configuration

--- examples/tsp3/run_simulation.sh
+++ examples/tsp3/run_simulation.sh
@@ -11,22 +11,16 @@ target/release/tsp3 -l 1.0 -s &
 
 sleep 2
 
+target/release/tsp3 -m 100 -i 100 -o 100 --method only_best &
+sleep $DELAY
 target/release/tsp3 -m 1 -i 1000 -o 100 --method simple &
 sleep $DELAY
-target/release/tsp3 -m 4 -i 1000 -o 100 --method simple &
+target/release/tsp3 -m 1 -i 10000 -o 100 --method simple &
 sleep $DELAY
-target/release/tsp3 -m 8 -i 1000 -o 100 --method simple &
+target/release/tsp3 -m 10 -i 1000 -o 100 --method simple &
 sleep $DELAY
-target/release/tsp3 -m 16 -i 1000 -o 100 --method simple &
-sleep $DELAY
-target/release/tsp3 -m 32 -i 1000 -o 100 --method simple &
-sleep $DELAY
-target/release/tsp3 -m 64 -i 1000 -o 100 --method simple &
-sleep $DELAY
-target/release/tsp3 -m 100 -i 1000 -o 100 --method only_best &
+target/release/tsp3 -m 1 -i 1000 -o 100 --method reset &
 sleep $DELAY
 target/release/tsp3 -m 1 -i 10000 -o 100 --method reset &
 sleep $DELAY
-target/release/tsp3 -m 10 -i 10000 -o 100 --method reset &
-sleep $DELAY
-target/release/tsp3 -m 100 -i 10000 -o 100 --method reset &
+target/release/tsp3 -m 10 -i 1000 -o 100 --method reset &
