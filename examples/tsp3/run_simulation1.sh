b2c142134af90ffbd0fb910b07f93d847c480944
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-08-16 18:01:33 +0200

    Rename run file

--- examples/tsp3/run_simulation1.sh
+++ examples/tsp3/run_simulation1.sh
@@ -4,24 +4,34 @@ killall tsp3
 
 rm *.log *.dat
 
+# Delay between clients to prevent overwriting log files
 DELAY=0.1
+
+# IP address of server
+IP=127.0.0.1
+
+# Population size
+POPULATION=100
 
 # maybe best: 89190.21485389463
 target/release/tsp3 -l 1.0 -o 100 -s &
 
+# Give the server a chance to start up
 sleep 2
 
-target/release/tsp3 -m 100 -i 100 -o 100 --method only_best &
+target/release/tsp3 --ip $IP -m 2 -i 1000 -o $POPULATION --method only_best &
 sleep $DELAY
-target/release/tsp3 -m 1 -i 1000 -o 100 --method simple &
+target/release/tsp3 --ip $IP -m 4 -i 1000 -o $POPULATION --method only_best &
 sleep $DELAY
-target/release/tsp3 -m 1 -i 10000 -o 100 --method simple &
+target/release/tsp3 --ip $IP -m 1 -i 1000 -o $POPULATION --method simple &
 sleep $DELAY
-target/release/tsp3 -m 10 -i 1000 -o 100 --method simple &
+target/release/tsp3 --ip $IP -m 2 -i 1000 -o $POPULATION --method simple &
 sleep $DELAY
-target/release/tsp3 -m 1 -i 1000 -o 100 --method reset &
+target/release/tsp3 --ip $IP -m 4 -i 1000 -o $POPULATION --method simple &
 sleep $DELAY
-target/release/tsp3 -m 1 -i 10000 -o 100 --method reset &
+target/release/tsp3 --ip $IP -m 1 -i 1000 -o $POPULATION --method reset &
 sleep $DELAY
-target/release/tsp3 -m 10 -i 1000 -o 100 --method reset &
+target/release/tsp3 --ip $IP -m 2 -i 1000 -o $POPULATION --method reset &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 4 -i 1000 -o $POPULATION --method reset &
 
