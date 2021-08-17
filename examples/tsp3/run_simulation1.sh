9fec50c4a2d64e134b6723f6234e0a45b8e526d4
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-08-17 16:48:37 +0200

    New run script configuration

--- examples/tsp3/run_simulation1.sh
+++ examples/tsp3/run_simulation1.sh
@@ -19,10 +19,6 @@ target/release/tsp3 -l 1.0 -o $POPULATION -s &
 # Give the server a chance to start up
 sleep 2
 
-target/release/tsp3 --ip $IP -m 2 -i 1000 -o $POPULATION --method only_best &
-sleep $DELAY
-target/release/tsp3 --ip $IP -m 4 -i 1000 -o $POPULATION --method only_best &
-sleep $DELAY
 target/release/tsp3 --ip $IP -m 1 -i 1000 -o $POPULATION --method simple &
 sleep $DELAY
 target/release/tsp3 --ip $IP -m 2 -i 1000 -o $POPULATION --method simple &
@@ -34,4 +30,11 @@ sleep $DELAY
 target/release/tsp3 --ip $IP -m 2 -i 1000 -o $POPULATION --method reset &
 sleep $DELAY
 target/release/tsp3 --ip $IP -m 4 -i 1000 -o $POPULATION --method reset &
-
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 10 -i 10000 -o $POPULATION --method random_delete &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 100 -i 10000 -o $POPULATION --method random_delete &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 10 -i 100 -o $POPULATION --method only_best &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 100 -i 100 -o $POPULATION --method only_best &
