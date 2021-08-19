14ff8054d092acff8aade09833276a4f62ca8315
Willi Kappler <willi.kappler@uni-tuebingen.de>
2021-08-19 15:33:35 +0200

    New default run configuration

--- examples/tsp3/run_simulation1.sh
+++ examples/tsp3/run_simulation1.sh
@@ -19,26 +19,19 @@ target/release/tsp3 -l 1.0 -o $POPULATION -s &
 # Give the server a chance to start up
 sleep 2
 
-target/release/tsp3 --ip $IP -m 1 -i 10000 -o $POPULATION --method simple &
+target/release/tsp3 --ip $IP -m 1 -i 8000 -o $POPULATION --method simple &
 sleep $DELAY
-target/release/tsp3 --ip $IP -m 10 -i 1000 -o $POPULATION --method simple &
+target/release/tsp3 --ip $IP -m 2 -i 4000 -o $POPULATION --method simple &
 sleep $DELAY
-target/release/tsp3 --ip $IP -m 10 -i 1000 -o $POPULATION --method only_best &
+target/release/tsp3 --ip $IP -m 4 -i 2000 -o $POPULATION --method simple &
 sleep $DELAY
-target/release/tsp3 --ip $IP -m 100 -i 100 -o $POPULATION --method only_best &
-
-#target/release/tsp3 --ip $IP -m 1 -r 20 -i 1000 -o $POPULATION --method simple &
-#sleep $DELAY
-#target/release/tsp3 --ip $IP -m 2 -i 1000 -o $POPULATION --method simple &
-#sleep $DELAY
-#target/release/tsp3 --ip $IP -m 10 -i 100 -o $POPULATION --method only_best &
-#sleep $DELAY
-#target/release/tsp3 --ip $IP -m 10 -r 20 -i 100 -o $POPULATION --method only_best &
-#sleep $DELAY
-#target/release/tsp3 --ip $IP -m 100 -i 100 -o $POPULATION --method only_best &
-#sleep $DELAY
-#target/release/tsp3 --ip $IP -m 10 -i 10000 -o $POPULATION --method random_delete &
-#sleep $DELAY
-#target/release/tsp3 --ip $IP -m 10 -r 20 -i 10000 -o $POPULATION --method random_delete &
-#sleep $DELAY
-#target/release/tsp3 --ip $IP -m 100 -i 10000 -o $POPULATION --method random_delete &
+target/release/tsp3 --ip $IP -m 8 -i 1000 -o $POPULATION --method simple &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 10 -i 800 -o $POPULATION --method only_best &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 20 -i 400 -o $POPULATION --method only_best &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 40 -i 200 -o $POPULATION --method only_best &
+sleep $DELAY
+target/release/tsp3 --ip $IP -m 80 -i 100 -o $POPULATION --method only_best &
+sleep $DELAY
